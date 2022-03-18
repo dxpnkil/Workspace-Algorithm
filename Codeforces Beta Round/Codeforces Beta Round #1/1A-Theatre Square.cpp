0de15b33133294eef54eee72a1a53e3b85c6dab2
Phạm Ngọc Dũng <85341341+dxpnkil@users.noreply.github.com>
2022-03-18 23:46:22 +0700

    Create 1A-Theatre Square.cpp

---
+++ Codeforces Beta Round/Codeforces Beta Round #1/1A-Theatre Square.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long m, n, a;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin >> m >> n >> a;
+    
+    cout << ((m+a-1)/a)*((n+a-1)/a);
+    return 0;
+}
