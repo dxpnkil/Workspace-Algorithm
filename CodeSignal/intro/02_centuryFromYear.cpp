c0dc4e41f8e51df1d966e608b8629c50fa78e933
Phạm Ngọc Dũng <85341341+dxpnkil@users.noreply.github.com>
2022-03-24 22:58:05 +0700

    Update 02_centuryFromYear.cpp

--- CodeSignal/intro/02_centuryFromYear.cpp
+++ CodeSignal/intro/02_centuryFromYear.cpp
@@ -1,7 +1,7 @@
 /* 
     Given a year, return the century it is in. 
     The first century spans from the year 1 up to and including the year 100,
-    spans from the year 1 up to and including the year 100, year 200, etc.
+        spans from the year 1 up to and including the year 100, year 200, etc.
     
     input:  year = 1905
     output: centuryFromYear(year) = 20
