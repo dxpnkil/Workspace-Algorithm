668b24ace274659fa6b839dd537731f8e6a498a6
Phạm Ngọc Dũng <85341341+dxpnkil@users.noreply.github.com>
2022-04-15 23:38:07 +0700

    Update 008_phoneCall.cpp

--- CodeSignal/the core/008_phoneCall.cpp
+++ CodeSignal/the core/008_phoneCall.cpp
@@ -1,9 +1,9 @@
 /*
   Some phone usage rate may be described as follows:
-
     first minute of a call costs min1 cents,
     each minute from the 2nd up to 10th (inclusive) costs min2_10 cents
     each minute after 10th costs min11 cents.
+    
   You have s cents on your account before the call. What is the duration of the longest call (in minutes rounded down to the nearest integer) you can have?
   
   input:  min1 = 3, min2_10 = 1, min11 = 2, and s = 20
