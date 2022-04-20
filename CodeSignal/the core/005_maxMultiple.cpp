62e895ef4b753b47131155d17f8ec4a7aea9f838
Phạm Ngọc Dũng <85341341+dxpnkil@users.noreply.github.com>
2022-04-20 23:36:58 +0700

    Update 005_maxMultiple.cpp

--- CodeSignal/the core/005_maxMultiple.cpp
+++ CodeSignal/the core/005_maxMultiple.cpp
@@ -1,9 +1,8 @@
 /*
   Given a divisor and a bound, find the largest integer N such that:
-
-  N is divisible by divisor.
-  N is less than or equal to bound.
-  N is greater than 0.
+    N is divisible by divisor.
+    N is less than or equal to bound.
+    N is greater than 0.
   It is guaranteed that such a number exists.
   
   input:  divisor = 3 and bound = 10
